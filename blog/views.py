7b1777f95ff07f61b14638dbcd532f6ecaa45d5d
Rostyslav <rostn25@gmail.com>
2023-09-03 18:19:34 +0200

    Added views to create/edit/delete blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
+from django.shortcuts import redirect
+from django.contrib import messages
 
 # Create your views here.
 
@@ -12,3 +15,42 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_list',)
+
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method =='POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            # post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+def delete_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == 'GET':
+        return render(request, 'blog/delete_post.html', {'post': post})
+    elif request.method == 'POST':
+        if 'cancel' in request.POST:
+            return redirect('post_detail', pk=post.pk)
+        else:
+            post.delete()
+            messages.success(request, 'The post has been deleted successfully.')
+            return redirect('post_list')
+
