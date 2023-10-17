a04c0033cd66ad7c558154f85e4b20a1170e9565
savage0003 <rostn25@gmail.com>
2023-10-17 19:23:11 +0100

    added comments

--- blog/urls.py
+++ blog/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('post/delete/<int:pk>/',views.delete_post,name='delete_post'),
     path('drafts/', views.post_draft_list, name='post_draft_list'),
     path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
 
 ]
